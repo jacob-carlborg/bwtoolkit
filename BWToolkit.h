805d65b04409b57d6aea4e5f0356db2820a4c865
Jacob Carlborg <jacob.carlborg@tretti.se>
2013-10-18 17:30:04 +0200

    Temp commit.

--- BWToolkit.h
+++ BWToolkit.h
@@ -6,8 +6,9 @@
 //  All code is provided under the New BSD license.
 //
 
-@interface BWToolkit {
+@protocol IDEInitialization
++ (BOOL)ide_initializeWithOptions:(int)arg1 error:(NSError **)arg2;
+@end
 
-}
-
+@interface BWToolkit : NSObject <IDEInitialization>
 @end
