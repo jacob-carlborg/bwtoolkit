937bf641831269036c117fd8ec60fee5326dc5a0
Jacob Carlborg <doob@me.com>
2013-10-20 15:16:41 +0200

    Temp 2

--- BWToolkit.m
+++ BWToolkit.m
@@ -12,25 +12,8 @@
 
 + (BOOL)ide_initializeWithOptions:(int)arg1 error:(NSError **)arg2
 {
-    NSLog(@"******** Initializing BWToolkit inferface buider plug-in");
 	return YES;
 }
-
-/*+ (void) load
-{
-    NSLog(@"******** Loading BWToolkit class");
-    NSString* frameworksPath = [[NSBundle bundleForClass:[BWToolkit class]] privateFrameworksPath];
-    NSBundle* bundle = [NSBundle bundleWithPath:[frameworksPath stringByAppendingPathComponent:@"BWTK.framework"]];
-
-    if (!bundle || ![bundle isLoaded])
-    {
-        if (!bundle || ![bundle load])
-            NSLog(@"******** Could not load BWTK.framework");
-
-        else
-            NSLog(@"******** Loaded BWTK.framework");
-    }
-}*/
 
 - (NSArray *)libraryNibNames {
     return [NSArray arrayWithObjects:@"BWSplitViewLibrary",@"BWControllersLibrary",@"BWToolbarItemsLibrary",@"BWBottomBarLibrary",@"BWToolkitLibrary",@"BWTransparentControlsLibrary",@"BWButtonBarLibrary",nil];
